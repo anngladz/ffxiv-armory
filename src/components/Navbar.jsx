6fe6d8896a81143138ed51c98ec721482381d253
anngladz <agladzinska@gmail.com>
2022-07-26 16:06:33 +0200

    Minor mobile fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
-import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
 const Navbar = () => {
   const [theme, setTheme] = useState('');
+  const [menu, setMenu] = useState(false);
 
   useEffect(() => {
     const savedTheme = localStorage.getItem('theme');
@@ -32,19 +32,62 @@ const Navbar = () => {
   return (
     <nav className='navbar bg-neutral text-neutral-content shadow-lg py-5'>
       <div className='container mx-auto'>
-        <Link to='/' className='btn btn-ghost normal-case text-xl'>
+        <a href='/' className='btn btn-ghost normal-case text-xl'>
           FFXIV Armory
-        </Link>
+        </a>
         <div className='ml-auto'>
-          <Link to='/' className='btn btn-ghost normal-case text-xl'>
-            Home
-          </Link>
-          <a
-            href='https://xivapi.com'
-            className='btn btn-ghost normal-case text-xl'
-          >
-            API
-          </a>
+          <div className='hidden 2xl:inline-block xl:inline-block lg:inline-block md:inline-block'>
+            <a href='/' className='btn btn-ghost normal-case text-xl'>
+              Home
+            </a>
+            <a
+              href='https://xivapi.com'
+              className='btn btn-ghost normal-case text-xl'
+            >
+              API
+            </a>
+          </div>
+          <label class='btn btn-circle swap swap-rotate -top-2 2xl:hidden'>
+            <input type='checkbox' onChange={() => setMenu((prev) => !prev)} />
+            <svg
+              className='swap-off fill-current'
+              xmlns='http://www.w3.org/2000/svg'
+              width='32'
+              height='32'
+              viewBox='0 0 512 512'
+            >
+              <path d='M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z' />
+            </svg>
+
+            <svg
+              className='swap-on fill-current'
+              xmlns='http://www.w3.org/2000/svg'
+              width='32'
+              height='32'
+              viewBox='0 0 512 512'
+            >
+              <polygon points='400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49' />
+            </svg>
+          </label>
+          {menu ? (
+            <div className='absolute top-28 left-0 bg-neutral bg-opacity-90 z-10 w-full h-full text-center'>
+              <a
+                href='/'
+                className='btn btn-ghost normal-case text-xl block h-10 leading-10'
+              >
+                Home
+              </a>
+              <a
+                href='https://xivapi.com'
+                className='btn btn-ghost normal-case text-xl block h-10 leading-10'
+              >
+                API
+              </a>
+            </div>
+          ) : (
+            ''
+          )}
+
           <label className='swap top-3 px-5'>
             <input type='checkbox' onChange={() => changeTheme(theme)} />
             {theme === 'night' ? (
